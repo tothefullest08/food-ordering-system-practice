f517949dbfe53b88e1a8dfa7b1238cae9abc8755
harry <tothefullest08@gmail.com>
2024-10-31 16:06:06 +0900

    feat: 주문 엔티티 - 주문 비즈니스 로직 구현
    주문 비즈니스 로직 구현
    주문 도메인 Exception 구현

--- order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/OrderItem.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/OrderItem.java
@@ -2,14 +2,25 @@ package com.food.ordering.system.order.service.domain.entity;
 
 import com.food.ordering.system.domain.entity.BaseEntity;
 import com.food.ordering.system.domain.valueobject.Money;
+import com.food.ordering.system.domain.valueobject.OrderId;
 import com.food.ordering.system.order.service.domain.valueobject.OrderItemId;
 
 public class OrderItem extends BaseEntity<OrderItemId> {
-    private OrderItemId orderItemId;
+    private OrderId orderId;
     private final Product product;
     private final int quantity;
     private final Money price;
     private final Money subTotal;
+
+
+    void initializeOrderItem(OrderId orderId, OrderItemId orderItemId) {
+        this.orderId = orderId;
+        super.setId(orderItemId);
+    }
+
+    boolean isPriceValid() {
+        return price.isGreaterThanZero() && price.equals(product.getPrice()) && price.multiply(quantity).equals(subTotal);
+    }
 
     private OrderItem(Builder builder) {
         super.setId(builder.orderItemId);
@@ -23,8 +34,8 @@ public class OrderItem extends BaseEntity<OrderItemId> {
         return new Builder();
     }
 
-    public OrderItemId getOrderItemId() {
-        return orderItemId;
+    public OrderId getOrderId() {
+        return orderId;
     }
 
     public Product getProduct() {
